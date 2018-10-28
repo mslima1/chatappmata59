c7d09987c222dd6dbc805c5337f0bf68d9716105
Matheus Lima <matheus_lima789@hotmail.com>
2018-10-27 22:10:03 -0300

    adicionando constantes, funções e broadcast de entrada/saida de usuário

--- cliente.py
+++ cliente.py
@@ -1,31 +1,31 @@
 import socket
 import threading
 
+MAX_SIZE = 1024
+ENCODE_TYPE = 'ascii'
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 3300
-
-
 ip = input('Digite o IP do servidor: ')
 
 s.connect((ip, port))
 nickname = input("Digite seu nome: ")
-s.send(nickname.encode('ascii'))
+s.send(nickname.encode(ENCODE_TYPE))
 
 clientOnline = True
 
 def receiveMsg(sckt):
     while clientOnline:
-            msg = sckt.recv(1024).decode('ascii')
+            msg = sckt.recv(MAX_SIZE).decode(ENCODE_TYPE)
             print(msg)
-
 
 threading.Thread(target = receiveMsg, args = (s,)).start()
 
 while clientOnline:
     aux = input()
-    msg = nickname + ':' + aux
+    msg = '[' + nickname +  ']' + ': ' + aux
     if '-q' in msg:
         clientOnline = False
-        s.send('-q'.encode('ascii'))
+        s.send('-q'.encode(ENCODE_TYPE))
     else:
-        s.send(msg.encode('ascii'))
+        s.send(msg.encode(ENCODE_TYPE))
--- servidor.py
+++ servidor.py
@@ -2,50 +2,58 @@ import socket
 import threading
 import time
 
+MAX_SIZE = 1024
+ENCODE_TYPE = 'ascii'
+
 s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-serverOnline = True
 ip = str (socket.gethostbyname (socket.gethostname ()))
 port = 3300
 
-clients = {}
-
 s.bind ((ip, port))
 s.listen ()
+
+clients = {}
+serverOnline = True
 print ('IP do servidor: %s' % ip)
 
+def broadcast(data):
+    for k, v in clients.items ():
+        v.send (data.encode (ENCODE_TYPE))
 
 def handleClient(client, nickname):
     clientConnected = True
     keys = clients.keys ()
     help = ('-w para lista de usuarios online; -q para se desconectar')
+    quitMsg = ('%s se desconectou do chat' %str(nickname))
+    numUsers = 'Usuarios conectados: \n'
 
     while clientConnected:
-            msg = client.recv (1024).decode ('ascii')
+            msg = client.recv (MAX_SIZE).decode (ENCODE_TYPE)
             print (time.ctime (time.time ()) + str (address) + ' ' + str (msg))
-            response = 'Usuarios conectados: \n'
             if '-w' in msg:
                 clientNo = 0
                 for name in keys:
-                    response = response + '-' + name + '\n'
-                client.send (response.encode ('ascii'))
+                    numUsers = numUsers + '-' + name + '\n'
+                client.send (numUsers.encode (ENCODE_TYPE))
             elif '-h' in msg:
-                client.send (help.encode ('ascii'))
+                client.send (help.encode (ENCODE_TYPE))
             elif '-q' in msg:
                 response = 'Desconectado'
-                client.send (response.encode ('ascii'))
+                client.send (response.encode (ENCODE_TYPE))
                 clients.pop (nickname)
                 print (nickname + ' se desconectou')
+                broadcast(quitMsg)
                 clientConnected = False
             else:
-                for k, v in clients.items ():
-                    v.send (msg.encode ('ascii'))
-
+                broadcast(msg)
 
 while serverOnline:
     client, address = s.accept ()
-    nickname = client.recv (1024).decode ('ascii')
+    nickname = client.recv (MAX_SIZE).decode (ENCODE_TYPE)
     print ('%s se conectou ao servidor' % str (nickname))
-    client.send ('Bem vindo ao chat! '.encode ('ascii'))
+    client.send ('Bem vindo ao chat! '.encode (ENCODE_TYPE))
+    welcomeMsg = ('%s se conectou ao chat' % str(nickname))
+    broadcast (welcomeMsg)
 
     if (client not in clients):
         clients[nickname] = client
