c1cb7dbf2941911d911c114e1ac746dd6ff9a3c7
Matheus Lima <matheus_lima789@hotmail.com>
2018-10-27 21:07:41 -0300

    1.0

--- cliente.py
+++ cliente.py
@@ -1,11 +1,35 @@
 import socket
+import threading
 
-socketCliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-porta = 3030
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 3300
+
 
 ip = input('Digite o IP do servidor: ')
-socketCliente.connect((ip, porta))
 
-msg = socketCliente.recv(1024)
-print(msg.decode('ascii'))
-socketCliente.close()
+s.connect((ip, port))
+nickname = input("Digite seu nome: ")
+s.send(nickname.encode('ascii'))
+
+clientOnline = True
+
+def receiveMsg(sock):
+    serverOffline = False
+    while clientOnline and (not serverOffline):
+        try:
+            msg = sock.recv(1024).decode('ascii')
+            print(msg)
+        except:
+            print('Servidor Offline')
+            serverDown = True
+
+threading.Thread(target = receiveMsg, args = (s,)).start()
+
+while clientOnline:
+    aux = input()
+    msg = nickname + ':' + aux
+    if '-q' in msg:
+        clientOnline = False
+        s.send('-q'.encode('ascii'))
+    else:
+        s.send(msg.encode('ascii'))
--- servidor.py
+++ servidor.py
@@ -1,17 +1,52 @@
 import socket
+import threading
 
-socketServidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-porta = 3030
-ipServidor = str(socket.gethostbyname(socket.gethostname()))
+s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+serverOnline = True
+ip = str (socket.gethostbyname (socket.gethostname ()))
+port = 3300
 
-socketServidor.bind((ipServidor, porta))
-socketServidor.listen()
+clients = {}
 
-servidorOnline= True
-print('Ip do servidor: %s' %ipServidor)
+s.bind ((ip, port))
+s.listen ()
+print ('IP do servidor: %s' % ip)
 
-while servidorOnline:
-    cliente, enderecoCliente = socketServidor.accept()
-    print('%s se conectou ao servidor' %str(enderecoCliente))
-    cliente.send('Bem vindo ao chat'.encode('ascii'))
-    cliente.close()
+
+def handleClient(client, nickname):
+    clientConnected = True
+    keys = clients.keys ()
+    help = ('-w para usuarios lista de online; -q para se desconectar')
+
+    while clientConnected:
+            msg = client.recv (1024).decode ('ascii')
+            response = 'Usuarios conectados: \n'
+            found = False
+            if '-w' in msg:
+                clientNo = 0
+                for name in keys:
+                    response = response + '-' + name + '\n'
+                client.send (response.encode ('ascii'))
+            elif '-h' in msg:
+                client.send (help.encode ('ascii'))
+            elif '-q' in msg:
+                response = 'Desconectado'
+                client.send (response.encode ('ascii'))
+                clients.pop (nickname)
+                print (nickname + ' se desconectou')
+                clientConnected = False
+            else:
+                for k, v in clients.items():
+                    v.send(msg.encode('ascii'))
+
+
+while serverOnline:
+    client, address = s.accept ()
+    nickname = client.recv (1024).decode ('ascii')
+    print ('%s se conectou ao servidor' % str (nickname))
+    client.send ('Bem vindo ao chat! '.encode ('ascii'))
+
+    if (client not in clients):
+        clients[nickname] = client
+        threading.Thread (target=handleClient, args=(client, nickname,)).start ()
+
