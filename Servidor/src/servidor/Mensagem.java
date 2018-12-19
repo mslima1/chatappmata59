a3cbcbc8ea50b71286187158584ce48badd539ad
Matheus Lima <matheus_lima789@hotmail.com>
2018-12-19 01:58:56 -0300

    rework

---
+++ Servidor/src/servidor/Mensagem.java
@@ -0,0 +1,81 @@
+package servidor;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Mensagem {
+
+    private Socket s;
+    private ArrayList<PrintStream> clientes;
+    
+    public Socket getS() {
+        return s;
+    }
+
+    public void setS(Socket s) {
+        this.s = s;
+    }
+
+    public ArrayList<PrintStream> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(ArrayList<PrintStream> clientes) {
+        this.clientes = clientes;
+    }
+
+    public Mensagem(Socket s,ArrayList<PrintStream> clientes){
+        this.s = s;
+        this.clientes = clientes;
+        Thread();
+    }
+    private void Thread(){
+        Thread t = new Thread (new Runnable() {
+            @Override
+            public void run() {
+                String mensagem = "";
+
+                try {
+                    InputStreamReader isr = new InputStreamReader(s.getInputStream());
+                    BufferedReader br = new BufferedReader(isr);
+
+                    while((mensagem = br.readLine())!= null){
+                        enviarMensagem(mensagem);
+
+                }
+
+
+                } catch (IOException ex) {
+                        ex.printStackTrace();
+                }
+
+                
+
+//                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        
+        t.start();
+                
+                
+        
+        
+    }
+    private void enviarMensagem(String mensagem){
+        for(int a = 0;a<clientes.size();a++){
+            
+            clientes.get(a).println(mensagem);
+            clientes.get(a).flush();
+            
+        }
+    }
+
+
+
+    
+}
---
+++ Servidor/src/servidor/Servidor.java
@@ -0,0 +1,52 @@
+
+package servidor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.*;
+import java.util.ArrayList;
+
+public class Servidor {
+
+
+    public static void main(String[] args) throws IOException {
+        String msgRec;
+        ArrayList<PrintStream> clientes = new ArrayList<>();
+        DatagramSocket ds = new DatagramSocket(10000);
+        byte [] data = new byte[100];
+        DatagramPacket pacote = new DatagramPacket(data, data.length);
+        try {
+            ServerSocket server = new ServerSocket(5000);
+            Socket socket;
+            
+            
+            while(true){
+                socket = server.accept();
+                ds.receive(pacote);
+                clientes.add(new PrintStream(socket.getOutputStream()));
+                
+                Mensagem mensagem = new Mensagem(socket,clientes);
+                ds.send(pacote);
+                
+                
+//                InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+//                BufferedReader br = new BufferedReader(isr);
+//                while((msgRec = br.readLine()) != null){
+//                    System.out.println(mensagem);
+//                }
+                
+                
+            }
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        
+        }
+
+
+    }
+    
+}
